9a7a7a8f547484b73a619ca951dabec46b27b923
HenriqueCSSilva <marcos_henrique@outlook.com>
2021-03-17 14:57:34 -0300

    14-57

--- variaveis.py
+++
@@ -1,3 +0,0 @@
-fonte = '<!DOCTYPE html><html lang="en"><head>    <meta charset="UTF-8">    <title>Title</title>' \
-        '</head><body> <p> ORÇAMENTISTA:</p><p>  OS:                            CLASSE DE TENSÃO: KV   ' \
-        '                              DATA:#HOJE	</p>  Descriçao -- Tabela -  </body></html>'
